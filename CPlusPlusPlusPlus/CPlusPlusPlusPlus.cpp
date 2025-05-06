55077664ee78394f9a962928360fbb41db65c1df
SmartDude18 <alexanderh1027@gmail.com>
2025-05-06 14:01:18 -0600

    added IO stuff for practice

--- CPlusPlusPlusPlus/CPlusPlusPlusPlus.cpp
+++ CPlusPlusPlusPlus/CPlusPlusPlusPlus.cpp
@@ -5,7 +5,10 @@
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    std::cout << "Hello World!\nWhat is your name?\n";
+    std::string playerInput = "";
+    std::cin >> playerInput;
+    std::cout << "Hello " + playerInput;
 }
 
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
